2de44acc02435457d3b172686990242c1ff4e4ae
Sherman Shi <shermanmishi@icloud.com>
2019-03-11 10:46:06 -0700

    Push ALL files, pods + FB

---
+++ Pods/Target Support Files/Pods-Wake Up Happy/Pods-Wake Up Happy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Wake_Up_Happy : NSObject
+@end
+@implementation PodsDummy_Pods_Wake_Up_Happy
+@end
---
+++ Pods/Target Support Files/Pods-Wake Up Happy/Pods-Wake Up Happy-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Wake_Up_HappyVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Wake_Up_HappyVersionString[];
+
