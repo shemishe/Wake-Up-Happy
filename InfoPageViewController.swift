7c3a53512c40b8c23655339d42a6e001b0b10e00
Sherman Shi <shermanmishi@icloud.com>
2019-03-16 19:47:00 -0700

    Created About and Info page with segue.

---
+++ InfoPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InfoPageViewController.swift
+//  Wake Up Happy
+//
+//  Created by Sherman Shi on 3/16/19.
+//  Copyright © 2019 Sherman Shi. All rights reserved.
+//
+
+import UIKit
+
+class InfoPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
